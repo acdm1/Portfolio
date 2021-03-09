17435776e836aab50dcaaf25d33dfa7ad3ce6483
acdm1 <acdm1@users.github.com>
2021-03-08 23:18:14 -0800

    fixed form glitch

--- Winter/504/Lab3/Lab3c/JS/main.js
+++ Winter/504/Lab3/Lab3c/JS/main.js
@@ -8,16 +8,19 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     // accessToken: 'pk.eyJ1IjoiYWNkbTEiLCJhIjoiY2toMmY5cmw0MDEyMDJ1bzR5eXZzam5paCJ9.Whiar4FcVdqRUV6VEG93Ig'
 }).addTo(map);
 
+
 var drawnItems = L.featureGroup().addTo(map);
 var cartoData = L.layerGroup().addTo(map);
 var url = "https://acdm1.carto.com/api/v2/sql";
 var urlGeoJSON = url + "?format=GeoJSON&q=";
-var sqlQuery = "SELECT the_geom, description, address, name FROM lab_3c_martin";
-
+// var sqlQuery = "SELECT the_geom, name, address, description, dfam, noise FROM lab_3c_martin";
+var sqlQuery = "SELECT * FROM lab_3c_martin";
 function addPopup(feature, layer) {
     layer.bindPopup(
-        "<b>" + feature.properties.name + "</b><br>" +
-        feature.properties.address + "<br>" +
+        "<b><u>" + feature.properties.name + "</u></b><br>" +
+        '<i>' + feature.properties.address + "</i><br>" +
+        '<i>' + feature.properties.dfam + "</i><br>" +
+        '<i>' + feature.properties.noise + "</i><br>" +
         feature.properties.description
     );
 }
@@ -32,7 +35,7 @@ fetch(urlGeoJSON + sqlQuery)
 
   new L.Control.Draw({
       draw : {
-          polygon : true,
+          polygon : false,
           polyline : false,      // Lines disabled
           rectangle : false,     // Rectangles disabled
           circle : false,        // Circles disabled
@@ -55,15 +58,12 @@ fetch(urlGeoJSON + sqlQuery)
   function createFormPopup() {
       var popupContent =
           '<form>' +
-
-          'Title:<br><input type="text" id="input_name"><br>' +
-
-          'Address:<br><input type="text" id="input_address"><br>' +
-
-          'Description:<br><input type="text" id="input_desc"><br>' +
-
+          'Username:<br><input type="text" id="input_name"><br>' +
+          'Park Section:<br><input type="text" id="input_address"><br>' +
+          'Description of Event:<br><input type="text" id="input_desc"><br>' +
+          'Duck Families Present:<br><input type="number" id="input_dfam" min="0" max="12"><br>' +
+          'Strange Noise Origin:<br><input type="text" id="input_noise"><br>' +
           '<input type="button" value="Submit" id="submit">' +
-
           '</form>'
       drawnItems.bindPopup(popupContent).openPopup();
   }
@@ -89,6 +89,8 @@ fetch(urlGeoJSON + sqlQuery)
           var enteredUsername = document.getElementById("input_name").value;
           var enteredDescription = document.getElementById("input_desc").value;
           var enteredAddress = document.getElementById("input_address").value;
+          var enteredDFam = document.getElementById("input_dfam").value;
+          var enteredNoise = document.getElementById("input_noise").value;
 
 // Print user name and description
 
@@ -107,12 +109,14 @@ fetch(urlGeoJSON + sqlQuery)
             // Create SQL expression to insert layer
                   var drawing = JSON.stringify(layer.toGeoJSON().geometry);
                   var sql =
-                      "INSERT INTO lab_3c_martin (the_geom, name, address, description) " +
+                      "INSERT INTO lab_3c_martin (the_geom, name, address, description, dfam, noise) " +
                       "VALUES (ST_SetSRID(ST_GeomFromGeoJSON('" +
                       drawing + "'), 4326), '" +
                       enteredUsername + "', '" +
                       enteredAddress + "', '" +
-                      enteredDescription + "')";
+                      enteredDescription + "', '" +
+                      enteredDFam + "', '" +
+                      enteredNoise + "')";
                   console.log(sql);
 
                   // Send the data
@@ -136,9 +140,11 @@ fetch(urlGeoJSON + sqlQuery)
               // Transfer submitted drawing to the CARTO layer
               //so it persists on the map without you having to refresh the page
               var newData = layer.toGeoJSON();
-              newData.properties.description = enteredDescription;
               newData.properties.name = enteredUsername;
               newData.properties.address = enteredAddress;
+              newData.properties.description = enteredDescription;
+              newData.properties.dfam = enteredDFam;
+              newData.properties.noise = enteredNoise;
               L.geoJSON(newData, {onEachFeature: addPopup}).addTo(cartoData);
 
           });
